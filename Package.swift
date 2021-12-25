8009c1d045793c94edd5b6bd5388882bd336d8b2
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-25 04:15:10 -0500

    Updated package

--- Package.swift
+++ Package.swift
@@ -4,24 +4,24 @@ let package = Package(
     name: "BluetoothWeb",
     platforms: [.macOS(.v12)],
     products: [
-        .executable(name: "BluetoothExplorer", targets: ["BluetoothExplorer"]),
-        .library(name: "BluetoothWeb", targets: ["BluetoothWeb"])
+        .executable(
+            name: "BluetoothExplorer",
+            targets: ["BluetoothExplorer"]
+        ),
+        .library(
+            name: "BluetoothWeb",
+            targets: ["BluetoothWeb"]
+        )
     ],
     dependencies: [
         .package(
             name: "Tokamak",
             url: "https://github.com/TokamakUI/Tokamak",
             from: "0.9.0"
-        ),/*
-        .package(
-            name: "Bluetooth",
-            path: "../Bluetooth"
-            //url: "https://github.com/PureSwift/Bluetooth.git",
-            //.branch("master")
-        ),*/
+        )
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "BluetoothExplorer",
             dependencies: [
                 .product(
@@ -31,10 +31,6 @@ let package = Package(
                 "BluetoothWeb"
             ]
         ),
-        /*
-        .target(
-            name: "Bluetooth"
-        ),*/
         .target(
             name: "BluetoothWeb",
             dependencies: [
@@ -42,16 +38,7 @@ let package = Package(
                     name: "TokamakShim",
                     package: "Tokamak"
                 ),
-                /*
-                .product(
-                    name: "JavaScriptKit",
-                    package: "JavaScriptKit"
-                ),*/
             ]
-        ),
-        .testTarget(
-            name: "BluetoothExplorerTests",
-            dependencies: ["BluetoothExplorer"]
-        ),
+        )
     ]
 )
