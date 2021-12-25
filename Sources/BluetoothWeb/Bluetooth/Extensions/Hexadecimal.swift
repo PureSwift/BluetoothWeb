733f3251804112a9bd8c51948120d9a1e51cb5ce
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-25 09:11:40 -0500

    Updated hexadecimal extensions

--- Sources/BluetoothWeb/Bluetooth/Extensions/Hexadecimal.swift
+++ Sources/BluetoothWeb/Bluetooth/Extensions/Hexadecimal.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 PureSwift. All rights reserved.
 //
 
+import Foundation
+
 internal extension FixedWidthInteger {
     
     func toHexadecimal() -> String {
@@ -14,7 +16,12 @@ internal extension FixedWidthInteger {
         while string.utf8.count < (MemoryLayout<Self>.size * 2) {
             string = "0" + string
         }
+        assert(string.utf8.count == MemoryLayout<Self>.size * 2)
         return string.uppercased()
+    }
+    
+    init?<T: StringProtocol>(hexadecimal string: T) {
+        self.init(string, radix: 16)
     }
 }
 
@@ -29,3 +36,33 @@ internal extension Collection where Element: FixedWidthInteger {
         return string
     }
 }
+
+internal extension Data {
+    
+    init?(hexadecimal string: String) {
+        let elementStringSize = MemoryLayout<Element>.size * 2 // 2 for UInt8
+        guard string.isEmpty == false else {
+            self.init()
+            return
+        }
+        guard string.count % elementStringSize == 0 else {
+            return nil
+        }
+        let elementsCount = string.count / elementStringSize
+        let elements = (0 ..< elementsCount)
+            .lazy
+            .map { ($0 * elementStringSize, ($0+1) * elementStringSize) }
+            .map { string.index(string.startIndex, offsetBy: $0.0) ..< string.index(string.startIndex, offsetBy: $0.1) }
+            .map { string[$0] }
+        self.init()
+        self.reserveCapacity(elementsCount)
+        for substring in elements {
+            guard let element = Element.init(hexadecimal: substring) else {
+                return nil
+            }
+            self.append(element)
+        }
+        assert(self.count == elementsCount)
+    }
+}
+
