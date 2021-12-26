17b380a6f673cb6d7d1c0092f52a29209d46951d
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-26 15:07:39 -0500

    Added `CentralError.errorDescription`

--- Sources/BluetoothWeb/GATT/CentralError.swift
+++ Sources/BluetoothWeb/GATT/CentralError.swift
@@ -26,7 +26,23 @@ public enum CentralError: Error {
 
 // MARK: - CustomNSError
 
-#if !os(WASI)
+#if os(WASI)
+extension CentralError: LocalizedError {
+    
+    public var errorDescription: String? {
+        switch self {
+        case .timeout:
+            return "The operation timed out."
+        case .disconnected:
+            return "The operation cannot be completed becuase the peripheral is disconnected."
+        case .unknownPeripheral:
+            return "Unknown peripheral."
+        case let .invalidAttribute(uuid):
+            return "Invalid attribute \(uuid)"
+        }
+    }
+}
+#else
 extension CentralError: CustomNSError {
     
     public enum UserInfoKey: String {
@@ -43,39 +59,25 @@ extension CentralError: CustomNSError {
     public var errorUserInfo: [String : Any] {
         
         var userInfo = [String: Any](minimumCapacity: 2)
-        
         switch self {
-            
         case .timeout:
-            
             let description = String(format: NSLocalizedString("The operation timed out.", comment: "org.pureswift.GATT.CentralError.timeout"))
-            
             userInfo[NSLocalizedDescriptionKey] = description
-            
         case .disconnected:
-            
             let description = String(format: NSLocalizedString("The operation cannot be completed becuase the peripheral is disconnected.", comment: "org.pureswift.GATT.CentralError.disconnected"))
-            
             userInfo[NSLocalizedDescriptionKey] = description
-            
         case .unknownPeripheral:
-            
             let description = String(format: NSLocalizedString("Unknown peripheral.", comment: "org.pureswift.GATT.CentralError.unknownPeripheral"))
-            
             userInfo[NSLocalizedDescriptionKey] = description
-            
         case let .invalidAttribute(uuid):
-            
             #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
             let description = String(format: NSLocalizedString("Invalid attribute %@.", comment: "org.pureswift.GATT.CentralError.invalidAttribute"), uuid.description)
             #else
             let description = "Invalid attribute \(uuid)"
             #endif
-            
             userInfo[NSLocalizedDescriptionKey] = description
             userInfo[UserInfoKey.uuid.rawValue] = uuid
         }
-        
         return userInfo
     }
 }
