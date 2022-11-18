1071ebed896709ba62acec289797a34ca40296cc
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-11-18 12:37:45 -0800

    Fixed characteristic data formatting

--- Sources/BluetoothExplorer/View/AttributeValueCell.swift
+++ Sources/BluetoothExplorer/View/AttributeValueCell.swift
@@ -65,29 +65,3 @@ extension AttributeValueCell {
         }
     }
 }
-
-internal extension BluetoothUUID {
-    
-    func description(for value: Data) -> String? {
-        // FIXME: Formatting
-        /*
-        switch self {
-        case .batteryLevel:
-            return value.first.flatMap { $0.description + "%" }
-        case .currentTime:
-            return nil
-        case .deviceName,
-            .serialNumberString,
-            .firmwareRevisionString,
-            .softwareRevisionString,
-            .hardwareRevisionString,
-            .modelNumberString,
-            .manufacturerNameString:
-            return String(data: value, encoding: .utf8)
-        default:
-            return nil
-        }*/
-        return nil
-    }
-}
-
